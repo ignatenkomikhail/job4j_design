3d41e1812048b45688160f2228d25b8123762bb4
ignatenkomikhail <igna@inbox.ru>
2020-07-30 19:03:17 +0300

    1. Реализовать коллекцию Set на массиве [#319598]

--- chapter_001/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_001/src/main/java/ru/job4j/set/SimpleSet.java
@@ -1,28 +1,22 @@
 package ru.job4j.set;
 
 import ru.job4j.generic.SimpleArray;
-
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.Objects;
 
 public class SimpleSet<T> implements Iterable<T> {
     private SimpleArray<T> data = new SimpleArray<>(3);
-    private int modCount = 0;
-    private int size = 0;
 
     public void add(T value) {
         if (notIncluded(value)) {
             data.add(value);
-            size++;
-            modCount++;
         }
     }
 
     private boolean notIncluded(T value) {
         boolean rslt = true;
         for (T el : data) {
-            if (value.equals(el)) {
+            if (Objects.equals(value, el)) {
                 rslt = false;
                 break;
             }
@@ -32,31 +26,6 @@ public class SimpleSet<T> implements Iterable<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return new It();
-    }
-
-    private class It implements Iterator<T> {
-        private int cursor = 0;
-        private int expectedModCount = 0;
-
-        {
-            expectedModCount = modCount;
-        }
-
-        @Override
-        public boolean hasNext() {
-            if (expectedModCount != modCount) {
-                throw new ConcurrentModificationException();
-            }
-            return cursor < size;
-        }
-
-        @Override
-        public T next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            return data.get(cursor++);
-        }
+        return data.iterator();
     }
 }
