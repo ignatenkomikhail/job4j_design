1f17a8695e4bddd81cb4358e3bbbc095786b0a6b
ignatenkomikhail <igna@inbox.ru>
2020-08-02 09:56:07 +0300

    1. Создать модель User [#319601]

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -3,9 +3,9 @@ package ru.job4j.map;
 import java.util.Calendar;
 
 public class User {
-    public String name;
-    public int children;
-    public Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
